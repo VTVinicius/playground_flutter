357e40be76604bb689cad3b2e06eb2ee69982e1f
Vinicius Teixeira <53088125+VTVinicius@users.noreply.github.com>
2023-03-25 11:59:20 -0300

    Dependencias corretas do UseCase nas ViewModels

--- lib/di/service_injection.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_bloc/flutter_bloc.dart';
-
-import '../core/network.dart';
-
-setupInjectionService() {
-  RepositoryProvider<NetworkManager>(
-    create: (context) => NetworkManager(),
-  );
-}
