3978d36fc07b6c4056b844fd2f1ebf0405b8b5a4
VTVinicius <53088125+VTVinicius@users.noreply.github.com>
2023-04-10 15:17:54 -0300

    Animated Container

--- lib/presentation/feature_experiments/animations/animated_container/animated_container_screen.dart
+++ lib/presentation/feature_experiments/animations/animated_container/animated_container_screen.dart
@@ -18,8 +18,43 @@ class AnimatedContainerScreen extends StatelessWidget {
           alignment: Alignment.center,
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
-            children: [],
+            children: const [
+              MyAnimatedContainerWidget(),
+            ],
           )),
+    );
+  }
+}
+class MyAnimatedContainerWidget extends StatefulWidget {
+  const MyAnimatedContainerWidget({super.key});
+
+  @override
+  State<MyAnimatedContainerWidget> createState() => _MyAnimatedContainerWidgetState();
+}
+
+class _MyAnimatedContainerWidgetState extends State<MyAnimatedContainerWidget> {
+  bool selected = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          selected = !selected;
+        });
+      },
+      child: Center(
+        child: AnimatedContainer(
+          width: selected ? 200.0 : 100.0,
+          height: selected ? 100.0 : 200.0,
+          color: selected ? Colors.red : Colors.blue,
+          alignment:
+          selected ? Alignment.center : AlignmentDirectional.topCenter,
+          duration: const Duration(seconds: 2),
+          curve: Curves.fastOutSlowIn,
+          child: const FlutterLogo(size: 75),
+        ),
+      ),
     );
   }
 }
