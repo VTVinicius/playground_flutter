b85f2a4d35daeb852e0861fef271b50ed66364c7
VTVinicius <53088125+VTVinicius@users.noreply.github.com>
2023-04-12 09:10:13 -0300

    Animated Physical Model

--- lib/presentation/feature_experiments/animations/animated_physical_model/animated_physical_model_screen.dart
+++ lib/presentation/feature_experiments/animations/animated_physical_model/animated_physical_model_screen.dart
@@ -18,8 +18,73 @@ class AnimatedPhysicalModelScreen extends StatelessWidget {
           alignment: Alignment.center,
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
-            children: [],
+            children: const [
+              MyAnimatedPhysicalModelWidget(),
+            ],
           )),
+    );
+  }
+}
+
+
+class MyAnimatedPhysicalModelWidget extends StatefulWidget {
+  const MyAnimatedPhysicalModelWidget({Key? key}) : super(key: key);
+  @override
+  _MyAnimatedPhysicalModelWidgetState createState() => _MyAnimatedPhysicalModelWidgetState();
+}
+class _MyAnimatedPhysicalModelWidgetState extends State<MyAnimatedPhysicalModelWidget> {
+  bool _play = false;
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+        children: [AnimatedPhysicalModel(
+          duration: const Duration(seconds: 2),
+          color: _play ? Colors.red : Colors.blue,
+          elevation: _play ? 9 : 3,
+          shape: BoxShape.rectangle,
+          shadowColor: Colors.deepOrangeAccent,
+          borderRadius: _play
+              ? const BorderRadius.all(Radius.circular(0))
+              : const BorderRadius.all(Radius.circular(24)),
+          child: Container(
+            width: 250,
+            height: 150,
+            child: const Center(
+                child: Text(
+                  'AnimatedPhysicalModel',
+                  style: TextStyle(fontSize: 18.0, color: Colors.white),
+                )),
+          ),
+        ),
+          const SizedBox(height: 50.0,),
+          AnimatedPhysicalModel(
+          duration: const Duration(seconds: 2),
+          color: Colors.blue,
+          elevation: _play ? 20 : 0,
+          shape: BoxShape.rectangle,
+          shadowColor: Colors.deepOrangeAccent,
+          borderRadius: const BorderRadius.all(Radius.circular(0)),
+          child: Container(
+            width: 250,
+            height: 150,
+            child: const Center(
+                child: Text(
+                  'AnimatedPhysicalModel',
+                  style: TextStyle(fontSize: 18.0, color: Colors.white),
+                )),
+          ),
+        ),
+
+      const SizedBox(height: 50.0,),
+      FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            _play = !_play;
+          });
+        },
+        child: Icon(Icons.play_arrow),
+      ),
+    ],
     );
   }
 }
