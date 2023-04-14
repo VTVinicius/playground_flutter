76a1ea2c6523715301ed0059bba48a24a2278b7b
VTVinicius <53088125+VTVinicius@users.noreply.github.com>
2023-04-14 15:36:22 -0300

    commit
    commit

--- lib/presentation/feature_experiments/animations/animated_positioned/animated_positioned_screen.dart
+++ lib/presentation/feature_experiments/animations/animated_positioned/animated_positioned_screen.dart
@@ -18,8 +18,53 @@ class AnimatedPositionedScreen extends StatelessWidget {
           alignment: Alignment.center,
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
-            children: [],
+            children: const [
+              MyAnimatedPositionedWidget(),
+            ],
           )),
+    );
+  }
+}
+
+class MyAnimatedPositionedWidget extends StatefulWidget {
+  const MyAnimatedPositionedWidget({super.key});
+
+  @override
+  State<MyAnimatedPositionedWidget> createState() =>
+      _MyAnimatedPositionedWidgetState();
+}
+
+class _MyAnimatedPositionedWidgetState
+    extends State<MyAnimatedPositionedWidget> {
+  bool selected = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: 200,
+      height: 350,
+      child: Stack(
+        children: <Widget>[
+          AnimatedPositioned(
+            width: selected ? 200.0 : 60.0,
+            height: selected ? 50.0 : 200.0,
+            top: selected ? 50.0 : 150.0,
+            duration: const Duration(seconds: 2),
+            curve: Curves.fastOutSlowIn,
+            child: GestureDetector(
+              onTap: () {
+                setState(() {
+                  selected = !selected;
+                });
+              },
+              child: Container(
+                color: Colors.blue,
+                child: const Center(child: Text('Tap me')),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
